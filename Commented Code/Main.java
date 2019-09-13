c7804c14e3994fe84b94f9c11a0dfd8492345387
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2019-09-13 15:02:44 +0530

    Commented Code
    Commented Code(\u000d)

---
+++ Commented Code/Main.java
@@ -0,0 +1,16 @@
+// cerner_2^5_2019
+
+//Though the below code is commented, it executes.
+
+// \u000d public class Main
+// \u000d {
+// \u000d	public static void main(String[] args) {
+		// \u000d System.out.println("Cerner_2^5_2019, program 1");
+		// \u000d System.out.println("Print the powers of 2 upto 2 ^ 5");
+		// \u000d int result = 1;
+		// \u000d for(int i=1;i<=5;i++)
+		// \u000d {  
+        // \u000d System.out.println(result = result * 2);  
+        // \u000d }  
+// \u000d	}
+// \u000d }
