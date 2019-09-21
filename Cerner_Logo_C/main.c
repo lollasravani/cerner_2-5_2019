ebdc024f15ce2b4642b41b2c9e2674c63dcdfc87
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2019-09-21 16:39:47 +0530

    Cerner_Logo(cerner_2^5_2019)
    Cerner_Logo(cerner_2^5_2019)

---
+++ Cerner_Logo_C/main.c
@@ -0,0 +1,6 @@
+//cerner_2^5_2019
+//This code prints Cerner Logo
+#include "stdio.h"
+void main(){int _,__,___;
+for(__=___=10; _="ghaiTYGdjhsuGDiD$#nahdjks_^&7gUY)d@#Gyi5r(BLRPPDJBPD\\`B\\B;`>B\\B\\DD\\FHBPPP<BLR"[__+++' '];)
+for(;_-->'@';)putchar( ++___=='*' ? ___ = (___-('*'-'('))/4:33^__&1);}
