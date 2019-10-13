48767c8a8b9234ac253b9e0c9430bdbd54bef02b
Lolla Sravani Lalitha <sravanilalitha.lolla@gmail.com>
2019-10-13 21:56:12 +0530

    IsPowerOfTwo_cpp(cerner_2^5_2019)
    IsPowerOfTwo_cpp(cerner_2^5_2019)

---
+++ IsPowerOfTwo_CPP/IsPowerOfTwo_cpp.cpp
@@ -0,0 +1,12 @@
+//cerner_2^5_2019
+//This programs prints if a number is a power of 2.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int _ = 32;
+    _ && (!(_&(_-1))) ? cout << "true" : cout << "false";
+    return 0;
+}
